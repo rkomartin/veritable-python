cbd43fa5953079a46fa92c1c13f64355476a0fed
Max Gasner <max@naviasystems.com>
2012-01-19 12:40:34 -0800

    add analyses

--- hierophant.py
+++ hierophant.py
@@ -23,7 +23,10 @@ def handle_http_error(r):
 
 def make_table_id():
 	return uuid.uuid4()
-	
+
+def make_row_id():
+	return uuid.uuid4()
+
 class VeritableConnection:
 	BASE_URL = "https://api.priorknowledge.com/tables"
 	
@@ -56,16 +59,12 @@ class VeritableAPI:
 	def tables(self):
 		"""Return the Veritable tables available to the user."""
 		r = self.connection.get()
-		ts = [VeritableTable(self.connection, t) for t in r["data"]]
-		return ts
+		return [VeritableTable(self.connection, t) for t in r["data"]]
 	
-	def create_table(self, table_id = make_table_id(), description):
+	def create_table(self, table_id = make_table_id(), description = ""):
 		"""Create a table with the given id."""	
-		if description is not None:
-			r = self.connection.put(self.connection.BASE_URL + "/" + table_id,
-									data = {"description": description})
-		else:
-			r = self.connection.put(self.connection.BASE_URL + "/" + table_id)
+		r = self.connection.put(self.connection.BASE_URL + "/" + table_id,
+								data = {"description": description})
 		return VeritableTable(self.connection, r)
 
 class VeritableTable:
@@ -79,7 +78,7 @@ class VeritableTable:
 					  "analyses": data["links"]["analyses"],
 					  "rows": data["links"]["rows"]}		
 
-	def get():
+	def get(self):
 		data = self.connection.get(self.links["self"])
 		if "description" in data:
 			self.description = data["description"]
@@ -90,29 +89,68 @@ class VeritableTable:
 					  "rows": data["links"]["rows"]}	
 		return self
 		
-	def delete():
-		r = self.connection.delete(self.links["self"])
-		return r
+	def delete(self):
+		return self.connection.delete(self.links["self"])
 		
-	def add_rows():
-		pass
+	def add_row(self, row):
+		if "_id" in row:
+			row_id = row["_id"]
+		else:
+			row_id = make_row_id()
+		return self.connection.put(self.links["rows"] + "/" + row_id, row)
 		
-	def get_row():
-		pass
+	def add_rows(self, data):
+		return self.connection.post(self.links["rows"], data)
 		
-	def list_analyses():
-		pass
+	def get_row(self, row_id):
+		return self.connection.get(self.links["rows"] + "/" + row_id)
+
+	def get_rows(self):
+		return self.connection.get(self.links["rows"])
 		
-class VeritableAnalysis:
-	def get():
-		pass
+	def delete_row(self, row_id):
+		return self.connection.delete(self.links["rows"] + "/" + row_id)
 		
-	def learn():
-		pass
-		
-	def delete():
-		pass
-		
-	def predict():
-		pass
+	def get_analyses(self):
+		r = self.connection.get(self.links["analyses"])
+		return [VeritableAnalysis(self.connection, a) for a in r["data"]]
 	
+	def create_analysis(self, schema, description = "",
+						analysis_id = make_analysis_id(), type = "veritable"):
+		r = self.connection.put(self.links["analyses"] + "/" + analysis_id,
+								data = {"description": description,
+										"type": type,
+										"schema": schema})
+class VeritableAnalysis:
+	def __init__(self, connection, data):
+		self.connection = connection
+		self.last_learned = data["last_learned"]
+		if "last_updated" in data
+			self.last_updated = data["last_updated"]
+		self.links = {"self": data["links"]["self"],
+					  "schema": data["links"]["schema"],
+					  "learn": data["links"]["learn"],
+					  "predict": data["links"]["predict"]}
+	def get(self):
+		data = self.connection.get(self.links["self"])
+		self.last_learned = data["last_learned"]
+		if "last_updated" in data
+			self.last_updated = data["last_updated"]
+		self.links = {"self": data["links"]["self"],
+					  "schema": data["links"]["schema"],
+					  "learn": data["links"]["learn"],
+					  "predict": data["links"]["predict"]}
+		return self
+		
+	def learn(self):
+		return self.connection.post(self.links["learn"])
+		
+	def delete(self):
+		return self.connection.delete(self.links["self"])
+
+	def get_schema(self):
+		return self.connection.get(self.links["schema"])
+		
+	def predict(self, request):
+		return self.connection.post(self.links["predict"], data = request)
+		
