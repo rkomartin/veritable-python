38d8505b983241ac32cb5bdaf1ed9b59d02a5ea1
Max Gasner <max@priorknowledge.net>
2012-01-20 11:41:33 -0800

    get the hex string representation, not the object

--- hierophant/utils.py
+++ hierophant/utils.py
@@ -1,13 +1,13 @@
 import uuid
 
 def make_table_id():
-    return uuid.uuid4()
+    return uuid.uuid4().hex
 
 def make_analysis_id():
-    return uuid.uuid4()
+    return uuid.uuid4().hex
 
 def make_row_id():
-    return uuid.uuid4()
+    return uuid.uuid4().hex
 
 def join_url(*args):
     return "/".join(args)
