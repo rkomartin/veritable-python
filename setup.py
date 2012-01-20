f956d3363b4932a3d0282e21359de38a90665940
Max Gasner <max@naviasystems.com>
2012-01-19 17:27:16 -0800

    reorganize for packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Hierophant',
+	  version='0.1',
+	  description='Python Client Library for Veritable API',
+	  author='Prior Knowledge, Inc.',
+	  author_email='developers@priorknowledge.com',
+	  url='http://www.priorknowledge.com/'
+	  packages=['hierophant'])
