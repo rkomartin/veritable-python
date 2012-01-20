4f6d10dad808cc1ec2671f37d0d797e0e65bf17a
Max Gasner <max@priorknowledge.net>
2012-01-20 12:57:43 -0800

    make sure operations can't be performed on deleted tables

--- hierophant/api.py
+++ hierophant/api.py
@@ -3,7 +3,14 @@ from hierophant.utils import *
 
 def veritable_connect(api_key):
     return API(Connection(api_key))
-            
+
+class DeletedTableException(Exception):
+    def __init__(self):
+        self.value = """"Cannot perform operations on a table that has already
+                         been deleted"""
+    def __str__(self):
+        return repr(self.value)
+
 class API:
     def __init__(self, connection):
         self.connection = connection
@@ -30,9 +37,15 @@ class Table:
         self.last_updated = data["last_updated"]
         self.links = {"self": data["links"]["self"],
                       "analyses": data["links"]["analyses"],
-                      "rows": data["links"]["rows"]}        
-
+                      "rows": data["links"]["rows"]}    
+                      
+    def still_alive(self):
+        """Check to make sure the table still exists."""
+        if self.has_been_deleted:
+            raise DeletedTableException()
+            
     def get(self):
+        self.still_alive()
         data = self.connection.get(self.links["self"])
         if "description" in data:
             self.description = data["description"]
@@ -44,9 +57,12 @@ class Table:
         return self
         
     def delete(self):
+        self.still_alive()
+        self.has_been_deleted = True
         return self.connection.delete(self.links["self"])
         
     def add_row(self, row):
+        self.still_alive()
         if "_id" in row:
             row_id = row["_id"]
         else:
@@ -54,23 +70,29 @@ class Table:
         return self.connection.put(join_url(self.links["rows"], row_id), row)
         
     def add_rows(self, data):
+        self.still_alive()
         return self.connection.post(self.links["rows"], data)
         
     def get_row(self, row_id):
+        self.still_alive()
         return self.connection.get(join_url(self.links["rows"], row_id))
 
     def get_rows(self):
+        self.still_alive()
         return self.connection.get(self.links["rows"])
         
     def delete_row(self, row_id):
+        self.still_alive()
         return self.connection.delete(join_url(self.links["rows"], row_id))
         
     def get_analyses(self):
+        self.still_alive()
         r = self.connection.get(self.links["analyses"])
         return [Analysis(self.connection, a) for a in r["data"]]
     
     def create_analysis(self, schema, description = "",
                         analysis_id = None, type = "veritable"):
+        self.still_alive()
         if analysis_id is None:
             analysis_id = make_analysis_id()
         r = self.connection.put(join_url(self.links["analyses"], analysis_id),
