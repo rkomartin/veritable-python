f415c1be4cdb539289fa27bb954d1a72d80e3559
Max Gasner <max@priorknowledge.net>
2012-01-20 14:34:47 -0800

    add minimal table creation test

--- test/test_hierophant.py
+++ test/test_hierophant.py
@@ -8,3 +8,8 @@ TEST_BASE_URL = "http://127.0.0.1:5000/tables"
 API = veritable_connect(TEST_API_KEY)
 API.connection.BASE_URL = TEST_BASE_URL
 
+API.tables()
+
+t1 = API.create_table()
+t2 = API.create_table("foo")
+t3 = API.create_table("foo", "A table of humbuggery")
