7f52c8ec62976c96ea44dd4a25d8046542fd64da
Vimal Bhalodia <vimal@priorknowledge.net>
2012-01-20 17:02:42 -0800

    Added support for custom SSL server certs or disabling ssl verification

--- hierophant/connection.py
+++ hierophant/connection.py
@@ -54,7 +54,7 @@ def murljoin(base,path):
     return urljoin(base,path.lstrip("/"))
 
 class Connection:
-    def __init__(self, api_key, api_base_url):
+    def __init__(self, api_key, api_base_url, ssl_verify=True):
         if api_key is None:
             raise APIKeyException()
         if api_base_url is None:
@@ -62,24 +62,25 @@ class Connection:
         self.api_key = api_key
         self.api_base_url = api_base_url.rstrip("/")+"/"
         self.auth = HTTPBasicAuth(self.api_key, self.api_key)
+        self.ssl_verify=ssl_verify
         
     def get(self, url):
         headers = {'accept-encoding': 'gzip'}
-        r = requests.get(murljoin(self.api_base_url,url), auth=self.auth, headers = headers)
+        r = requests.get(murljoin(self.api_base_url,url), verify=self.ssl_verify, auth=self.auth, headers = headers)
         return get_response_data(r)
     
     def post(self, url, data):
         headers = {'content-type': 'application/json', 'content-encoding': 'gzip'}
-        r = requests.post(murljoin(self.api_base_url,url), data=mgzip(json.dumps(data)), auth=self.auth,
+        r = requests.post(murljoin(self.api_base_url,url), verify=self.ssl_verify, data=mgzip(json.dumps(data)), auth=self.auth,
                              headers = headers)
         return get_response_data(r)
     
     def put(self, url, data):
         headers = {'content-type': 'application/json', 'content-encoding': 'gzip'}
-        r = requests.put(murljoin(self.api_base_url,url), data=mgzip(json.dumps(data)), auth = self.auth,
+        r = requests.put(murljoin(self.api_base_url,url), verify=self.ssl_verify, data=mgzip(json.dumps(data)), auth = self.auth,
                             headers = headers)
         return get_response_data(r)
         
     def delete(self, url):
-        r = requests.delete(murljoin(self.api_base_url,url), auth=self.auth)
+        r = requests.delete(murljoin(self.api_base_url,url), verify=self.ssl_verify, auth=self.auth)
         return get_response_data(r)
