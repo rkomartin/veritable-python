ae76422081b9157eb95bef6dfa0a891e3fdc3d0d
Max Gasner <max@priorknowledge.net>
2012-01-20 17:14:41 -0800

    refactor url munging

--- hierophant/connection.py
+++ hierophant/connection.py
@@ -1,20 +1,22 @@
-from requests.auth import HTTPBasicAuth
 import requests
 import simplejson as json
 from gzip import GzipFile
 from io import BytesIO
 from requests.auth import HTTPBasicAuth
 from urlparse import urljoin
+from hierophant.utils import format_url
 
 class APIKeyException(Exception):
     def __init__(self):
-        self.value = """Must provide an API key to instantiate a Veritable connection"""
+        self.value = """Must provide an API key to instantiate a Veritable
+                        connection"""
     def __str__(self):
         return repr(self.value)
 
 class APIBaseURLException(Exception):
     def __init__(self):
-        self.value = """Must provide an base URL to instantiate a Veritable connection"""
+        self.value = """Must provide an base URL to instantiate a Veritable
+                        connection"""
     def __str__(self):
         return repr(self.value)
 
@@ -50,9 +52,6 @@ def mgzip(buf):
     wbuf.close()
     return result
 
-def murljoin(base,path):
-    return urljoin(base,path.lstrip("/"))
-
 class Connection:
     def __init__(self, api_key, api_base_url):
         if api_key is None:
@@ -60,26 +59,31 @@ class Connection:
         if api_base_url is None:
             raise APIBaseURLException()        
         self.api_key = api_key
-        self.api_base_url = api_base_url.rstrip("/")+"/"
+        self.api_base_url = format_url(api_base_url)
         self.auth = HTTPBasicAuth(self.api_key, self.api_key)
         
     def get(self, url):
         headers = {'accept-encoding': 'gzip'}
-        r = requests.get(murljoin(self.api_base_url,url), auth=self.auth, headers = headers)
+        r = requests.get(format_url(self.api_base_url, url), auth=self.auth,
+                           headers = headers)
         return get_response_data(r)
     
     def post(self, url, data):
-        headers = {'content-type': 'application/json', 'content-encoding': 'gzip'}
-        r = requests.post(murljoin(self.api_base_url,url), data=mgzip(json.dumps(data)), auth=self.auth,
-                             headers = headers)
+        headers = {'content-type': 'application/json',
+                     'content-encoding': 'gzip'}
+        r = requests.post(format_url(self.api_base_url,url), 
+                            data=mgzip(json.dumps(data)), auth=self.auth,
+                            headers = headers)
         return get_response_data(r)
     
     def put(self, url, data):
-        headers = {'content-type': 'application/json', 'content-encoding': 'gzip'}
-        r = requests.put(murljoin(self.api_base_url,url), data=mgzip(json.dumps(data)), auth = self.auth,
-                            headers = headers)
+        headers = {'content-type': 'application/json',
+                     'content-encoding': 'gzip'}
+        r = requests.put(format_url(self.api_base_url,url), 
+                           data=mgzip(json.dumps(data)), auth = self.auth,
+                           headers = headers)
         return get_response_data(r)
         
     def delete(self, url):
-        r = requests.delete(murljoin(self.api_base_url,url), auth=self.auth)
+        r = requests.delete(format_url(self.api_base_url,url), auth=self.auth)
         return get_response_data(r)
