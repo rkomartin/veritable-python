9e32d33a5b5015d8e7c9dd7e87250ce52668a1d0
Max Gasner <max@priorknowledge.net>
2012-01-23 12:12:34 -0800

    flesh out integration tests

--- test/test_hierophant.py
+++ test/test_hierophant.py
@@ -1,7 +1,8 @@
 #! usr/bin/python
 
 from requests.auth import HTTPBasicAuth
-from hierophant.api import veritable_connect
+from hierophant.api import veritable_connect, DeletedTableException
+from requests.exceptions import HTTPError
 
 TEST_API_KEY = "test"
 TEST_BASE_URL = "http://127.0.0.1:5000"
@@ -20,15 +21,15 @@ try:
 	t4.get()
 except DeletedTableException:
 	pass
-	
+
 try:
 	t4 = API.get_table_by_id("foo")
-except requests.exceptions.HTTPError
+except HTTPError:
 	pass
 
 ts2 = API.tables()
 
-[t.still_alive() for t in ts]
+[t.still_alive() for t in ts2]
 
 t5 = API.create_table("foo")
 t5 = API.create_table("foo")
@@ -36,7 +37,7 @@ t5 = API.create_table("foo")
 t5.add_row({'furry': 'true', 'hooves': 'false', 'size': 2})
 t5.add_row({'_id': 'camel', 'furry': 'true', 'hooves': 'true', 'size': 4})
 t5.add_row({'_id': 'camel', 'furry': 'true', 'hooves': 'true', 'size': 4})
-t5.add_row({'_id': 'lizard', 'furry': 'fa;se', 'hooves': 'false', 'size': 1})
+t5.add_row({'_id': 'lizard', 'furry': 'false', 'hooves': 'false', 'size': 1})
 t5.get_rows()
 t5.get_row('camel')
 t5.get_row('lizard')
@@ -44,6 +45,10 @@ t5.delete_row('lizard')
 t5.delete_row('lizard')
 t5.get_row('lizard')
 t5.delete_row('camel')
+t5.get_rows()
+
+t6 = t5.get()
+t5.last_updated == t6.last_updated
 
 #add_rows
 #get - compare timestamps
