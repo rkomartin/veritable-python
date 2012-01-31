763c32291148ece0302a67ac500c308668f59551
Max Gasner <max@priorknowledge.net>
2012-01-31 11:55:01 -0800

    package mammals tests into test class

--- test/test_mammals.py
+++ test/test_mammals.py
@@ -8,75 +8,81 @@ DATA_FILE = "mammals.json"
 
 data = json.load(open(DATA_FILE, 'r'))
 
-API = veritable_connect(TEST_API_KEY, TEST_BASE_URL)
 
 def wait_for_analysis(analysis, poll=10):
-	while analysis.status() is "pending":
+	while analysis.status() == "pending":
 		time.sleep(poll)
-	if analysis.status() is "failed":
+	if analysis.status() == "failed":
 		raise Exception(analysis.get_state()["error"])
 
-def test_end_to_end():
-	# Create the table
-	mammals = API.create_table("mammals", force=True)
-	mammals.add_rows(data)
-	schema_1 = {
-		'furry': {'type': 'boolean'},
-		'hairless': {'type': 'boolean'},
-		'big': {'type': 'boolean'},
-		'small': {'type': 'boolean'},
-		'flippers': {'type': 'boolean'},
-		'hands': {'type': 'boolean'},
-		'hooves': {'type': 'boolean'},
-		'paws': {'type': 'boolean'},
-		'longneck': {'type': 'boolean'},
-		'tail': {'type': 'boolean'},
-		'filter_feeder': {'type': 'boolean'},
-		'horns': {'type': 'boolean'},
-		'claws': {'type': 'boolean'},
-		'tusks': {'type': 'boolean'},
-		'flys': {'type': 'boolean'},
-		'tunnels': {'type': 'boolean'},
-		'walks': {'type': 'boolean'},
-		'nocturnal': {'type': 'boolean'},
-		'eats_fish': {'type': 'boolean'},
-		'eats_meat': {'type': 'boolean'},
-		'eats_plankton': {'type': 'boolean'},
-		'eats_vegetation': {'type': 'boolean'},
-		'grazer': {'type': 'boolean'},
-		'hunter': {'type': 'boolean'},
-		'scavenger': {'type': 'boolean'},
-		'lives_in_arctic': {'type': 'boolean'},
-		'lives_on_coast': {'type': 'boolean'},
-		'lives_on_plains': {'type': 'boolean'},
-		'lives_in_forest': {'type': 'boolean'},
-		'lives_in_jungle': {'type': 'boolean'},
-		'lives_in_ocean': {'type': 'boolean'},
-		'lives_on_ground': {'type': 'boolean'},
-		'lives_in_water': {'type': 'boolean'},
-		'lives_in_trees': {'type': 'boolean'},
-		'herd': {'type': 'boolean'},
-		'family': {'type': 'categorical'},
-		'order': {'type': 'categorical'},
-		'exotic': {'type': 'categorical'}
-	}
-	validate_schema(schema_1)
-	analysis_1 = mammals.create_analysis(schema_1, description="""Full mammals
-	  analysis, exotic coded categorical""", analysis_id="mammals_1")
-	analysis_1.run()
+class TestMammals:
+	def setup():
+		self.API = veritable_connect(TEST_API_KEY, TEST_BASE_URL)
+		self.mammals = self.API.create_table("mammals", force=True)
+		self.mammals.add_rows(data)
+		self.schema = {
+			'furry': {'type': 'boolean'},
+			'hairless': {'type': 'boolean'},
+			'big': {'type': 'boolean'},
+			'small': {'type': 'boolean'},
+			'flippers': {'type': 'boolean'},
+			'hands': {'type': 'boolean'},
+			'hooves': {'type': 'boolean'},
+			'paws': {'type': 'boolean'},
+			'longneck': {'type': 'boolean'},
+			'tail': {'type': 'boolean'},
+			'filter_feeder': {'type': 'boolean'},
+			'horns': {'type': 'boolean'},
+			'claws': {'type': 'boolean'},
+			'tusks': {'type': 'boolean'},
+			'flys': {'type': 'boolean'},
+			'tunnels': {'type': 'boolean'},
+			'walks': {'type': 'boolean'},
+			'nocturnal': {'type': 'boolean'},
+			'eats_fish': {'type': 'boolean'},
+			'eats_meat': {'type': 'boolean'},
+			'eats_plankton': {'type': 'boolean'},
+			'eats_vegetation': {'type': 'boolean'},
+			'grazer': {'type': 'boolean'},
+			'hunter': {'type': 'boolean'},
+			'scavenger': {'type': 'boolean'},
+			'lives_in_arctic': {'type': 'boolean'},
+			'lives_on_coast': {'type': 'boolean'},
+			'lives_on_plains': {'type': 'boolean'},
+			'lives_in_forest': {'type': 'boolean'},
+			'lives_in_jungle': {'type': 'boolean'},
+			'lives_in_ocean': {'type': 'boolean'},
+			'lives_on_ground': {'type': 'boolean'},
+			'lives_in_water': {'type': 'boolean'},
+			'lives_in_trees': {'type': 'boolean'},
+			'herd': {'type': 'boolean'},
+			'family': {'type': 'categorical'},
+			'order': {'type': 'categorical'},
+			'exotic': {'type': 'categorical'}
+		}
 
-	schema_2 = schema_1
-	schema_2["exotic"]["type"] = "real"
-	validate_schema(schema_2)
-	analysis_2 = mammals.create_analysis(schema_2, description="""Full mammals
-	  analysis, exotic coded real""", analysis_id="mammals_2")
-	analysis_2.run()
+	def test_run_mammals_exotic_as_categorical():
+		schema = self.schema
+		validate_schema(schema)
+		analysis = self.mammals.create_analysis(schema, description="""Full
+			mammals analysis, exotic coded as categorical""",
+			analysis_id="mammals_1")
+		analysis.run()
+	
+	def test_run_mammals_exotic_as_real():
+		schema = self.schema
+		schema["exotic"]["type"] = "real"
+		validate_schema(schema)
+		analysis = self.mammals.create_analysis(schema, description="""Full
+			mammals analysis, exotic coded as categorical""",
+			analysis_id="mammals_1")
+		analysis.run()
 
-	wait_for_analysis(analysis_1)
-	request_1 = {'data': {'furry': True, 'small': True, 'walks': False,
-				 'flys': 'null', 'exotic': 'null'}, 'count': 10}
-	analysis_1.predict(request_1)
-
-	wait_for_analysis(analysis_2)
-	request_2 = {'data': {'claws': True, 'herd': False, 'eats_meat': True,
-				 'exotic': 'null'}, 'count': 10}
+	def test_run_mammals_exotic_as_count():
+		schema = self.schema
+		schema["exotic"]["type"] = "count"
+		validate_schema(schema)
+		analysis = self.mammals.create_analysis(schema, description="""Full
+			mammals analysis, exotic coded as categorical""",
+			analysis_id="mammals_1")
+		analysis.run()
