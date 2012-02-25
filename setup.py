e5c36c469ae13f737a1154d02993692629200a6f
Vimal Bhalodia <vimal@priorknowledge.net>
2012-02-24 17:38:31 -0800

    Updated setup logic to read version number from version.py Logic for reading from version.py based on advice: http://blog.yjl.im/2012/02/why-your-python-library-needs-separate.html

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
+import os
 from distutils.core import setup
 
-setup(name='veritable',
-	  version='1.0.0',
+def get_version():
+    basedir = os.path.dirname(__file__)
+    with open(os.path.join(basedir, 'veritable/version.py')) as f:
+        __version__ = None
+        exec(f.read())
+        return __version__
+
+if __name__ == '__main__':
+    setup(name='veritable',
+	  version=get_version(),
 	  description='Python Client Library for Veritable API',
 	  author='Prior Knowledge, Inc.',
 	  author_email='developers@priorknowledge.com',
