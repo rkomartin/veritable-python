a58dc13a7efb165f4b25700c484183ce1f9ac9df
Cap Petschulat <zfcd21@gmail.com>
2012-02-27 15:57:18 -0800

    Base all errors on VeritableError.

--- veritable/exceptions.py
+++ veritable/exceptions.py
@@ -1,84 +1,87 @@
-class APIKeyException(Exception):
+class VeritableError(Exception):
+    pass
+
+class APIKeyException(VeritableError):
     def __init__(self):
         self.value = """Must provide an API key to instantiate a Veritable
                         connection"""
     def __str__(self):
         return repr(self.value)
 
-class APIBaseURLException(Exception):
+class APIBaseURLException(VeritableError):
     def __init__(self):
         self.value = """Must provide an base URL to instantiate a Veritable
                         connection"""
     def __str__(self):
         return repr(self.value)
 
-class APIConnectionException(Exception):
+class APIConnectionException(VeritableError):
     def __init__(self, url):
         self.value = """No Veritable server at """ + url
     def __str__(self):
         return repr(self.value)
 
-class DeletedTableException(Exception):
+class DeletedTableException(VeritableError):
     def __init__(self):
         self.value = """"Cannot perform operations on a table that has already been deleted"""
     def __str__(self):
         return repr(self.value)
 
-class DeletedAnalysisException(Exception):
+class DeletedAnalysisException(VeritableError):
     def __init__(self):
         self.value = """"Cannot perform operations on an analysis that has already been deleted"""
     def __str__(self):
         return repr(self.value)
 
-class MissingRowIDException(Exception):
+class MissingRowIDException(VeritableError):
     def __init__(self):
         self.value = """Rows must contain row ids in the _id field"""
     def __str__(self):
         return repr(self.value)
 
-class InvalidAnalysisTypeException(Exception):
+class InvalidAnalysisTypeException(VeritableError):
     def __init__(self):
         self.value = """Invalid analysis type."""
     def __str__(self):
         return repr(self.value)
 
-class InvalidSchemaException(Exception):
+class InvalidSchemaException(VeritableError):
     def __init__(self):
         self.value = """Invalid schema specification."""
     def __str__(self):
         return repr(self.value)
 
-class DuplicateTableException(Exception):
+class DuplicateTableException(VeritableError):
     def __init__(self, table_id):
         self.value = "Table with id " + table_id + " already exists! Set force=True to override."
     def __str__(self):
         return repr(self.value)
 
-class DuplicateAnalysisException(Exception):
+class DuplicateAnalysisException(VeritableError):
     def __init__(self, analysis_id):
         self.value = "Analysis with id " + analysis_id + " already exists! Set force=True to override."
     def __str__(self):
         return repr(self.value)
 
-class AnalysisNotReadyException(Exception):
+class AnalysisNotReadyException(VeritableError):
     def __init__(self):
         self.value = "Analysis is not ready for predictions."
     def __str__(self):
         return repr(self.value)
 
-class ServerException(Exception):
+class ServerException(VeritableError):
     def __init__(self, val="unknown"):
         self.value = "Error reported by server: " + val
     def __str__(self):
         return repr(self.value)
 
-class InvalidPredictionRequest(Exception):
+class InvalidPredictionRequest(VeritableError):
     def __init__(self, msg):
         self.value = msg
     def __str__(self):
         return repr(self.value)
 
-class DataValidationException(Exception):
+class DataValidationException(VeritableError):
     def __init__(self, msg, row=None, field=None):
         self.value = msg
         self.row = row
