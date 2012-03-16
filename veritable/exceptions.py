53ce7ed4f81df8678a3effb0737e9df87ff8a973
Max Gasner <max@priorknowledge.com>
2012-03-16 10:28:37 -0700

    document exceptions and remove unused exceptions

--- veritable/exceptions.py
+++ veritable/exceptions.py
@@ -1,8 +1,11 @@
 class VeritableError(Exception):
+    """Base class for exceptions in veritable-python."""
     pass
 
 
 class APIKeyException(VeritableError):
+    """Raised if an API key is not provided when instantiating a Veritable
+        connection."""
     def __init__(self):
         self.value = """Must provide an API key to instantiate a Veritable
             connection"""
@@ -12,8 +15,10 @@ class APIKeyException(VeritableError):
 
 
 class APIBaseURLException(VeritableError):
+    """Raised if a base URL is not provided when instantiating a Veritable
+        connection."""
     def __init__(self):
-        self.value = """Must provide an base URL to instantiate a Veritable
+        self.value = """Must provide a base URL to instantiate a Veritable
             connection"""
 
     def __str__(self):
@@ -21,6 +26,7 @@ class APIBaseURLException(VeritableError):
 
 
 class APIConnectionException(VeritableError):
+    """Raised if a Veritable server is not found at the base URL."""
     def __init__(self, url):
         self.value = """No Veritable server at """ + url
 
@@ -28,25 +34,8 @@ class APIConnectionException(VeritableError):
         return repr(self.value)
 
 
-class DeletedTableException(VeritableError):
-    def __init__(self):
-        self.value = """"Cannot perform operations on a table that has
-            already been deleted"""
-
-    def __str__(self):
-        return repr(self.value)
-
-
-class DeletedAnalysisException(VeritableError):
-    def __init__(self):
-        self.value = """"Cannot perform operations on an analysis that has
-            already been deleted"""
-
-    def __str__(self):
-        return repr(self.value)
-
-
 class MissingRowIDException(VeritableError):
+    """Raised when a row ID is not provided."""
     def __init__(self):
         self.value = """Rows must contain row ids in the _id field"""
 
@@ -55,6 +44,7 @@ class MissingRowIDException(VeritableError):
 
 
 class InvalidIDException(VeritableError):
+    """Raised if an invalid ID is provided."""
     def __init__(self, s=None):
         if s:
             self.value = """Specified id """ + s + """ is invalid:
@@ -68,6 +58,7 @@ class InvalidIDException(VeritableError):
 
 
 class InvalidAnalysisTypeException(VeritableError):
+    """Raised if an invalid analysis type is specified."""
     def __init__(self):
         self.value = """Invalid analysis type."""
 
@@ -76,6 +67,7 @@ class InvalidAnalysisTypeException(VeritableError):
 
 
 class InvalidSchemaException(Exception):
+    """Raised if an invalid schema is provided."""
     def __init__(self, msg="""Invalid schema specification.""", col=None):
         self.value = msg
         self.col = col
@@ -85,6 +77,8 @@ class InvalidSchemaException(Exception):
 
 
 class DuplicateTableException(VeritableError):
+    """Raised if an attempt is made to create a table that already exists,
+        without overwriting it."""
     def __init__(self, table_id):
         self.value = """Table with id """ + table_id + """ already exists!
             Set force=True to override."""
@@ -94,6 +88,8 @@ class DuplicateTableException(VeritableError):
 
 
 class DuplicateAnalysisException(VeritableError):
+    """Raised if an attempt is made to create an analysis that already exists,
+        without overwriting it."""
     def __init__(self, analysis_id):
         self.value = """Analysis with id """ + analysis_id + """ already
             exists! Set force=True to override."""
@@ -102,15 +98,8 @@ class DuplicateAnalysisException(VeritableError):
         return repr(self.value)
 
 
-class AnalysisNotReadyException(VeritableError):
-    def __init__(self):
-        self.value = """Analysis is not ready for predictions."""
-
-    def __str__(self):
-        return repr(self.value)
-
-
 class ServerException(VeritableError):
+    """Raised when an error is returned by the Veritable server."""
     def __init__(self, val="unknown"):
         self.value = """Error reported by server: """ + val
 
@@ -118,15 +107,9 @@ class ServerException(VeritableError):
         return repr(self.value)
 
 
-class InvalidPredictionRequest(VeritableError):
-    def __init__(self, msg):
-        self.value = msg
-
-    def __str__(self):
-        return repr(self.value)
-
-
 class DataValidationException(VeritableError):
+    """Raised when invalid data is passed to the validation utility
+        functions."""
     def __init__(self, msg, row=None, col=None):
         self.value = msg
         self.row = row
