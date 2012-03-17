eb9e90ad6323b6d942b751f4e72a2037af03644e
Max Gasner <max@priorknowledge.com>
2012-03-16 18:16:48 -0700

    distinguish limit from count

--- veritable/cursor.py
+++ veritable/cursor.py
@@ -11,8 +11,10 @@ class Cursor:
     See also: https://dev.priorknowledge.com/docs/client/python
 
     """
-    def __init__(self, connection, collection, start=None, per_page=35):
+    def __init__(self, connection, collection, start=None, per_page=35,
+        limit=None):
         self.__data = []
+        self.__limit = limit
         self.__next = None
         self.__last = False
         self.__start = start
@@ -49,8 +51,10 @@ class Cursor:
 
     def next(self):
         if len(self.__data) or self._refresh():
+            if self.__limit is not None:
+                if self.__limit == 0:
+                    raise StopIteration
+                self.__limit -= 1
             return self.__data.pop(0)
         else:
             raise StopIteration
-
-            # distinguish limit from count
