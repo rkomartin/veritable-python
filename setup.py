65c3b508adf224017f963d1e355ac49d444559cf
Max Gasner <max@priorknowledge.com>
2012-06-07 15:30:30 -0700

    remove non-ascii dash

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     setup(name='veritable',
       version=get_version(),
       description='Python Client Library for Veritable API',
-      long_description='Veritable is a system for understanding and making predictions about tabular data — i.e., structured data that can be organized into rows and columns. Its basic inputs are rows of data, which it uses to learn a joint predictive model. Its outputs are predictions about the values of new rows: given the values of any subset of columns, Veritable can predict the values of any other columns.',
+      long_description='Veritable is a system for understanding and making predictions about tabular data -- i.e., structured data that can be organized into rows and columns. Its basic inputs are rows of data, which it uses to learn a joint predictive model. Its outputs are predictions about the values of new rows: given the values of any subset of columns, Veritable can predict the values of any other columns.',
       author='Prior Knowledge, Inc.',
       author_email='developers@priorknowledge.com',
       maintainer='Max Gasner',
