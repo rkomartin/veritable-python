fdf01013f59f0408782fdaf3e048de7e2de01043
Max Gasner <max@priorknowledge.com>
2012-06-07 15:24:01 -0700

    classifiers and long_description for pypi

--- setup.py
+++ setup.py
@@ -13,10 +13,21 @@ if __name__ == '__main__':
     setup(name='veritable',
       version=get_version(),
       description='Python Client Library for Veritable API',
+      long_description='Veritable is a system for understanding and making predictions about tabular data — i.e., structured data that can be organized into rows and columns. Its basic inputs are rows of data, which it uses to learn a joint predictive model. Its outputs are predictions about the values of new rows: given the values of any subset of columns, Veritable can predict the values of any other columns.',
       author='Prior Knowledge, Inc.',
       author_email='developers@priorknowledge.com',
       maintainer='Max Gasner',
       maintainer_email='max@priorknowledge.com',
       url='http://www.priorknowledge.com/',
       install_requires=['requests'],
-      packages=['veritable'])
+      packages=['veritable'],
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Database',
+        'Topic :: Scientific/Engineering :: Information Analysis'
+      ])
