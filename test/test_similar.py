c1a2d8d0c8778fb23c23f4381028475802769132
Vimal Bhalodia <vimal@priorknowledge.net>
2012-07-09 17:50:22 -0700

    consolidated test setup

--- test/test_similar.py
+++ test/test_similar.py
@@ -34,8 +34,6 @@ class TestSimilar:
     def setup_class(self):
         self.API = veritable.connect(TEST_API_KEY, TEST_BASE_URL,
             **connect_kwargs)
-
-    def setup(self):
         self.t = self.API.create_table()
         self.t.batch_upload_rows(
         [{'_id': 'row1', 'cat': 'a', 'ct': 0, 'real': 1.02394, 'bool': True},
@@ -52,32 +50,37 @@ class TestSimilar:
                   }
         self.a = self.t.create_analysis(self.schema, analysis_id="a1",
             force=True)
+        self.a.wait()
+
+    @classmethod
+    def teardown_class(self):
+        self.t.delete()
+
+
+    def setup(self):
+        pass
 
     def teardown(self):
-        self.t.delete()
+        pass
 
     @attr('async')
     def test_similar_to(self):
-        self.a.wait()
         for col in self.schema.keys():
             for row in self.t.get_rows():
                 self.a.similar_to({'_id': row['_id']}, col, 1)
 
     @attr('async')
     def test_similar_to_with_invalid_column_fails(self):
-        self.a.wait()
         assert_raises(VeritableError, self.a.similar_to,
           {'_id': 'row1'}, 'missing-col', 1)
         
     @attr('async')
     def test_similar_to_with_invalid_row_fails(self):
-        self.a.wait()
         assert_raises(VeritableError, self.a.similar_to,
           {'_id': 'missing-row:'}, 'ct', 1)
 
     @attr('async')
     def test_similar_to_return_data(self):
-        self.a.wait()
         [assert_in('ct', r[0].keys()) for r in self.a.similar_to({'_id': 'row1'}, 'cat', 1,
                                                              return_data=True)]
         [assert_not_in('ct', r[0].keys()) for r in self.a.similar_to({'_id': 'row1'}, 'cat', 1,
@@ -85,22 +88,18 @@ class TestSimilar:
 
     @attr('async')
     def test_similar_to_link_is_present(self):
-        self.a.wait()
         self.a._link('similar')
 
     @attr('async')
     def test_similar_to_result(self):
-        self.a.wait()
         assert(len([r for r in self.a.similar_to({'_id': 'row1'}, 'cat', 1)]) <= 1)
 
     @attr('async')
     def test_similar_to_result_limit_0(self):
-        self.a.wait()
         assert(len([r for r in self.a.similar_to({'_id': 'row1'}, 'cat',
             max_rows=0)]) == 0)
 
     @attr('async')
     def test_similar_to_result_limit_3(self):
-        self.a.wait()
         assert(len([r for r in self.a.similar_to({'_id': 'row1'}, 'cat',
             max_rows=3)]) <= 3)
