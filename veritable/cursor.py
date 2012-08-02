8d4b6420f5af3e6efe778e0e3a96e927d71c8811
Jonathan Glidden <jonathan@priorknowledge.net>
2012-08-01 17:42:28 -0700

    key argument for transform of data

--- veritable/cursor.py
+++ veritable/cursor.py
@@ -14,21 +14,20 @@ class Cursor:
     See also: https://dev.priorknowledge.com/docs/client/python
 
     """
-    def __init__(self, connection, collection, key=None, start=None,
+    def __init__(self, connection, collection, key=lambda x: x, start=None,
                  per_page=100, limit=None, extra_args={}):        
         self.__limit = limit
         self.__start = start
         self.__per_page = per_page
         self.__connection = connection
         self.__collection = collection
+        self.__f = key
         collection_key = collection.split("/")[-1]
         params = {'per_page': self.__per_page,
                   'start': self.__start}
         params.update(extra_args)
         res = self.__connection.get(self.__collection, params=params)
-        if key:
-            self.__key = key
-        elif collection_key in res:
+        if collection_key in res:
             self.__key = collection_key
         else:
             self.__key = 'data'
@@ -80,6 +79,6 @@ class Cursor:
                 if self.__limit == 0:
                     raise StopIteration
                 self.__limit -= 1
-            return self.__data.pop(0)
+            return self.__f(self.__data.pop(0))
         else:
             raise StopIteration
