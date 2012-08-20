10a372eb40ceba7aa9ecc656251a56414cbc66a2
Cap Petschulat <zfcd21@gmail.com>
2012-08-20 11:22:13 -0700

    Respect new requests behavior by not passing None-valued parameters.
    Prior to requests 0.13.7, requests ignored None-valued query
    parameters. Starting in 0.13.7, it includes these in the query string
    as the string None. This works around the new behavior by simply not
    including query parameters if they are None.

--- veritable/cursor.py
+++ veritable/cursor.py
@@ -22,8 +22,11 @@ class Cursor:
         self.__connection = connection
         self.__collection = collection
         collection_key = collection.split("/")[-1]
-        params = {'per_page': self.__per_page,
-                  'start': self.__start}
+        params = {}
+        if self.__per_page is not None:
+            params['per_page'] = self.__per_page
+        if self.__start is not None:
+            params['start'] = self.__start
         res = self.__connection.get(self.__collection, params=params)
         if collection_key in res:
             self.__key = collection_key
